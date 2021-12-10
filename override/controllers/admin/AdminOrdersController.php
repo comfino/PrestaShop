c9f7f7df114bf1aece667c7e432397b8ff895c01
akozubski@comperia.pl <akozubski@comperia.pl>
2021-12-10 14:55:30 +0100

    FN-8406 Fix bug in cancel notifications under PS 1.6 - alternative logic of sending order cancellations to the Comfino API for PrestaShop 1.6.

--- override/controllers/admin/AdminOrdersController.php
+++ override/controllers/admin/AdminOrdersController.php
@@ -90,12 +90,16 @@ class AdminOrdersController extends AdminOrdersControllerCore
                                 $this->errors[] = $this->displayWarning(sprintf('Order #%d has already been assigned this status.', $id_order));
                             } else {
                                 if ($current_order_state->id != $order_state->id) {
-                                    if ($current_order_state !== null && $current_order_state->id != $order_state->id && $current_order_state->name === 'Canceled' && count($order->getOrderPayments())) {
-                                        if ($order->payment === 'Comfino payments' && ($current_order_state->paid || in_array($current_order_state->name, $this->comfinoConfirmStates, true))) {
+                                    if ($current_order_state !== null && $current_order_state->id != $order_state->id && $order_state->id == Configuration::get('PS_OS_CANCELED') && count($order->getOrderPayments())) {
+                                        if ($order->payment === 'Comfino payments' && ($current_order_state->paid || count(array_intersect($current_order_state->name, $this->comfinoConfirmStates)))) {
                                             $this->errors[] = sprintf(Tools::displayError('Cancellation of accepted order #%d paid via Comfino service is not allowed.'), $id_order);
 
                                             unset($_POST['orderBox'][$idx]);
                                         }
+                                    }
+
+                                    if (isset($_POST['orderBox'][$idx]) && $order_state->id == Configuration::get('PS_OS_CANCELED')) {
+                                        ComfinoApi::cancelOrder($order->id);
                                     }
                                 } else {
                                     $this->errors[] = Tools::displayError('The order has already been assigned this status.');
@@ -134,10 +138,14 @@ class AdminOrdersController extends AdminOrdersControllerCore
                     $current_order_state = $order->getCurrentOrderState();
 
                     if ($current_order_state->id != $order_state->id) {
-                        if ($current_order_state !== null && $current_order_state->id != $order_state->id && $current_order_state->name === 'Canceled' && count($order->getOrderPayments())) {
-                            if ($order->payment === 'Comfino payments' && ($current_order_state->paid || in_array($current_order_state->name, $this->comfinoConfirmStates, true))) {
+                        if ($current_order_state !== null && $current_order_state->id != $order_state->id && $order_state->id == Configuration::get('PS_OS_CANCELED') && count($order->getOrderPayments())) {
+                            if ($order->payment === 'Comfino payments' && ($current_order_state->paid || count(array_intersect($current_order_state->name, $this->comfinoConfirmStates)))) {
                                 $this->errors[] = Tools::displayError('Cancellation of accepted order paid via Comfino service is not allowed.');
                             }
+                        }
+
+                        if (empty($this->errors) && $order_state->id == Configuration::get('PS_OS_CANCELED')) {
+                            ComfinoApi::cancelOrder($order->id);
                         }
                     } else {
                         $this->errors[] = Tools::displayError('The order has already been assigned this status.');
