c10f2f719b00013272c341668d7c572f73d287ea
akozubskicr <83001403+akozubskicr@users.noreply.github.com>
2021-12-14 10:57:16 +0100

    Set proper version value (#23)
    * FN-8417 Fix some compatibility bugs with PrestaShop 1.7.7.5.
    
    * FN-8417 Remove bug concerned with order cancellation and notification to the Comfino API.
    
    * FN-8417 Update version.

--- comfino.php
+++ comfino.php
@@ -38,7 +38,7 @@ if (!defined('COMFINO_PS_17')) {
 }
 
 if (!defined('COMFINO_VERSION')) {
-    define('COMFINO_VERSION', '2.0.5');
+    define('COMFINO_VERSION', '2.0.2');
 }
 
 if (COMFINO_PS_17) {
