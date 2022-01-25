6fa64aabc81e095ec14b7e7c1100cc027f92b42e
akozubskicr <83001403+akozubskicr@users.noreply.github.com>
2022-01-25 14:27:16 +0100

    FN-8568 Fix bug in payment controller (wrong include path constant). (#28)

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -29,8 +29,8 @@ if (!defined('_PS_VERSION_')) {
     return;
 }
 
-require_once '.'._MODULE_DIR_.'comfino/src/Api.php';
-require_once '.'._MODULE_DIR_.'comfino/models/OrdersList.php';
+require_once _PS_MODULE_DIR_.'comfino/src/Api.php';
+require_once _PS_MODULE_DIR_.'comfino/models/OrdersList.php';
 
 class ComfinoPaymentModuleFrontController extends ModuleFrontController
 {
