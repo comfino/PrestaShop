f675a8ea1b76b77966fc53171901c03f420670f3
akozubski@comperia.pl <akozubski@comperia.pl>
2022-11-22 11:26:14 +0100

    FN-9849 Add external dependency - library with SHA3 PHP implementation, update notification endpoint.

--- controllers/front/notify.php
+++ controllers/front/notify.php
@@ -23,6 +23,9 @@
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
+
+use desktopd\SHA3\Sponge as SHA3;
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -40,7 +43,7 @@ class ComfinoNotifyModuleFrontController extends ModuleFrontController
 
         $jsonData = Tools::file_get_contents('php://input');
 
-        if ($this->getSignature() !== hash('sha3-256', ComfinoApi::getApiKey() . $jsonData)) {
+        if ($this->getSignature() !== $this->hash(ComfinoApi::getApiKey() . $jsonData)) {
             exit($this->setResponse(400, 'Failed comparison of CR-Signature and shop hash.'));
         }
 
@@ -70,5 +73,23 @@ class ComfinoNotifyModuleFrontController extends ModuleFrontController
         header('Content-Type: application/json');
 
         return json_encode(['status' => $content]);
+    }
+
+    private function hash($inputString)
+    {
+        $hash = null;
+
+        if (in_array('sha3-256', hash_algos(), true)) {
+            $hash = hash('sha3-256', $inputString);
+        } else {
+            require_once _PS_MODULE_DIR_ . 'comfino/lib/php-sha3-streamable/namespaced/desktopd/SHA3/Sponge.php';
+
+            $sponge = SHA3::init(SHA3::SHA3_256);
+            $sponge->absorb($inputString);
+
+            $hash = bin2hex($sponge->squeeze());
+        }
+
+        return $hash;
     }
 }
