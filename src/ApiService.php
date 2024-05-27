b1411e833805e3f3695931b945907c6dfc409d03
akozubski@comperia.pl <akozubski@comperia.pl>
2024-05-27 10:27:19 +0200

    FN-11336 Plugin refactoring - ApiService updated.

--- src/ApiService.php
+++ src/ApiService.php
@@ -2,12 +2,15 @@
 
 namespace Comfino;
 
+use Comfino\Api\Exception\ServiceUnavailable;
 use Comfino\Common\Backend\Factory\ApiServiceFactory;
 use Comfino\Common\Backend\RestEndpoint\Configuration;
 use Comfino\Common\Backend\RestEndpoint\StatusNotification;
 use Comfino\Common\Backend\RestEndpointManager;
 use Comfino\Common\Shop\Order\StatusManager;
 use Comfino\Order\StatusAdapter;
+use Psr\Http\Message\ResponseInterface;
+use Sunrise\Http\Factory\ResponseFactory;
 
 final class ApiService
 {
@@ -45,5 +48,28 @@ final class ApiService
                 ...ConfigManager::getEnvironmentInfo(['shop_version', 'plugin_version', 'database_version'])
             )
         );
+    }
+
+    public static function processRequest(): string
+    {
+        if (self::$endpointManager === null || empty(self::$endpointManager->getRegisteredEndpoints())) {
+            http_response_code(503);
+
+            return 'Endpoint manager not initialized.';
+        }
+
+        $response = self::$endpointManager->processRequest();
+
+        foreach ($response->getHeaders() as $headerName => $headerValues) {
+            foreach ($headerValues as $headerValue) {
+                header(sprintf('%s: %s', $headerName, $headerValue), false);
+            }
+        }
+
+        $responseBody = $response->getBody()->getContents();
+
+        http_response_code($response->getStatusCode());
+
+        return !empty($responseBody) ? $responseBody : $response->getReasonPhrase();
     }
 }
